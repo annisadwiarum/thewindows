e75b6e438980c755dae270a5bdd16f4ef1fc4e25
annisadwiarum <annisadwiarum710@gmail.com>
2024-01-16 17:06:44 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Layout>
         <div className='items-center justify-center'></div>
         <h1 className={`font-bold p-10 text-xl text-center md:text-2xl`}>
-         Enjoy Your Times
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas iusto neque ipsum illo facilis maxime eligendi, nemo vero. Corporis sunt blanditiis, minima rem veritatis odit! Corporis, quod perferendis? Ea fugit perspiciatis quas nihil sed assumenda qui sapiente? Vero cum unde nisi, sint magni sed. Iusto velit excepturi accusamus quidem dolor eius nesciunt modi? Nihil cumque quo nobis ut. Eius et quod aut ullam aliquam. Rerum qui architecto assumenda! Quisquam itaque quae ducimus dicta? Aspernatur maiores possimus illum. Quod, officiis. Dignissimos eaque minus placeat magnam a distinctio ducimus ipsum, dolores numquam cum atque suscipit excepturi autem odio officiis minima adipisci omnis ratione ullam harum molestiae vero temporibus illum ea. Enim soluta eius et debitis dolore omnis ipsa totam. Blanditiis at ab recusandae quisquam nostrum vero eveniet possimus, laudantium nemo inventore similique cupiditate eum sed? Vel rerum sapiente quam veniam est ipsum aliquid ea quos harum eligendi dolores sed temporibus, necessitatibus nulla veritatis quia, consequatur dolore suscipit pariatur eius. Reprehenderit, ratione minima nesciunt repellat similique eligendi cum! Tempore, esse hic. Rerum assumenda quo ab cumque veritatis cupiditate natus voluptatum sapiente et quidem reprehenderit sequi vitae eveniet, repellat minus maxime aliquid dolor aspernatur labore laboriosam officiis beatae quis odit. Veritatis magni placeat aut.
         </h1>
         <h1 className={`font-bold p-10 text-xl text-center md:text-2xl`}>
          Enjoy Your Times
